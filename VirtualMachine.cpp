0cb3fb848999c9dae312b185062cc42848d7990d
Ian Nicholas Hurd <hurdian@pc9.cs.ucdavis.edu>
2015-04-21 16:58:32 -0700

    fileCallback() helper function added
    VMFileOpen() probably working; VMFileClose() and VMFileSeek() added

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -4,6 +4,7 @@
 
 extern "C" TVMMainEntry VMLoadModule(const char *module);
 extern "C" void VMUnloadModule(void);
+void fileCallback(void* calldata, int result);
 void incrementTicks(void*);
 
 volatile unsigned int ticks;
@@ -29,18 +30,37 @@ TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[])
 
 TVMStatus VMFileOpen(const char *filename, int flags, int mode, int *filedescriptor)
 {
+  *filedescriptor = -1; //impossible to have a negative file descriptor
   if (filename == NULL || filedescriptor == NULL)
   {
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }
   //change thread state to VM_THREAD_STATE_WAITING
-  MachineFileOpen(filename, flags, mode, filedescriptor, NULL);
+  MachineFileOpen(filename, flags, mode, fileCallback, (void*)filedescriptor);
   // void MachineFileOpen(const char *filename, int flags, int mode, TMachineFileCallback callback, void *calldata);
-
+  while(*filedescriptor < 0); //wait until file is actually open
   //change thread state to VM_THREAD_STATE_READY
   return VM_STATUS_SUCCESS;
 //  return VM_STATUS_FAILURE;
 } // VMFileOpen
+
+
+TVMStatus VMFileClose(int filedescriptor)
+{
+  int data = 1;
+
+  // Make thread wait here.
+  MachineFileClose(filedescriptor, fileCallback, (void*)data);
+  while (data > 0); //wait until data has been altered
+  // Change thread state to Stop waiting.
+
+  if ( data < 0 )
+  {
+    return VM_STATUS_FAILURE;
+  }//negative result is a failure
+
+  return VM_STATUS_SUCCESS;
+}
 
 
 TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
@@ -56,6 +76,25 @@ TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
   }
   return VM_STATUS_FAILURE;
 } // VMFileWrite
+
+
+TVMStatus VMFileSeek(int filedescriptor, int offset, int whence, int *newoffset)
+{
+  // void MachineFileSeek(int fd, int offset, int whence, TMachineFileCallback callback, void *calldata);
+  int no = -728;
+
+  // Make thread wait
+  MachineFileSeek(filedescriptor, offset, whence, fileCallback, (void*)&no);
+
+  while (no == -728);
+  if (newoffset)
+  {
+    no = *newoffset;
+  }
+  //make thread stop waiting
+
+  return VM_STATUS_SUCCESS;
+}//TVMStatus VMFileseek(int filedescriptor, int offset, int whence, int *newoffset)
 
 
 TVMStatus VMThreadSleep(TVMTick tick)
@@ -79,6 +118,11 @@ TVMStatus VMThreadSleep(TVMTick tick)
 }
 
 
+void fileCallback(void* calldata, int result)
+{
+//MachineFileOpen callback function
+  calldata = (void*)result;
+}//void fileOpen(void* calldata, int result)
 
 
 void incrementTicks(void*)
