865c0c3b08a2cf18209bda63da17d2f2be6e11c2
Samuel Weber <sbweber@pc5.cs.ucdavis.edu>
2015-04-30 17:15:58 -0700

    Added signal suspension and resumption to make VM functions atomic
    Tested very thoroughly until it worked properly.

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -58,9 +58,11 @@ TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[])
 
 TVMStatus VMFileOpen(const char *filename, int flags, int mode, int *filedescriptor)
 {
+  MachineSuspendSignals(&sigs);
   tr->setcd(-18); //impossible to have a negative file descriptor
   if (filename == NULL || filedescriptor == NULL)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }//need to have a filename and a place to put the FD
   MachineFileOpen(filename, flags, mode, fileCallback, (void*)tr);
@@ -68,15 +70,18 @@ TVMStatus VMFileOpen(const char *filename, int flags, int mode, int *filedescrip
   scheduler();
   if(tr->getcd() < 0)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_FAILURE;
   }//if invalid FD
   *filedescriptor = tr->getcd();
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 } // VMFileOpen
 
 
 TVMStatus VMFileClose(int filedescriptor)
 {
+  MachineSuspendSignals(&sigs);
   tr->setcd(1);
   // Make thread wait here.
   MachineFileClose(filedescriptor, fileCallback, (void*)tr);
@@ -84,17 +89,21 @@ TVMStatus VMFileClose(int filedescriptor)
   scheduler();
   if (tr->getcd() < 0)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_FAILURE;
   }//negative result is a failure
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMFileClose(int filedescriptor)
 
 
 TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
 {
+  MachineSuspendSignals(&sigs);
   tr->setcd(-739);
   if (!data || !length)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }//not allowed to have NULL pointers for where we put the data
   MachineFileWrite(filedescriptor, data, *length, fileCallback, (void*)tr);
@@ -102,14 +111,17 @@ TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
   scheduler();
   if(tr->getcd() < 0)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_FAILURE;
   }//if a non-negative number of bytes written
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 } // VMFileWrite
 
 
 TVMStatus VMFileSeek(int filedescriptor, int offset, int whence, int *newoffset)
 {
+  MachineSuspendSignals(&sigs);
   tr->setcd(-728);
   MachineFileSeek(filedescriptor, offset, whence, fileCallback, (void*)tr);
   tr->setState(VM_THREAD_STATE_WAITING);
@@ -117,30 +129,43 @@ TVMStatus VMFileSeek(int filedescriptor, int offset, int whence, int *newoffset)
   if (newoffset)
   {
     *newoffset = tr->getcd();
-  }
+  }//return newoffset
   if (tr->getcd() < 0)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_FAILURE;
+  }//check for failure
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMFileseek(int filedescriptor, int offset, int whence, int *newoffset)
 
 
 TVMStatus VMFileRead(int filedescriptor, void *data, int *length)
 {
+  MachineSuspendSignals(&sigs);
   tr->setcd(-728);
   if (!data || !length)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
+  }//not allowed to be NULL pointers
   MachineFileRead(filedescriptor, data, *length, fileCallback, (void*)tr);
   tr->setState(VM_THREAD_STATE_WAITING);
   scheduler();
   *length = tr->getcd();
   if (tr->getcd() < 0)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_FAILURE;
+  }//if the calldata was invalid
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMFileseek(int filedescriptor, int offset, int whence, int *newoffset)
 
 
 TVMStatus VMThreadSleep(TVMTick tick)
 {
+  MachineSuspendSignals(&sigs);
   if (tick == VM_TIMEOUT_IMMEDIATE)
   {
     tr->setState(VM_THREAD_STATE_READY);
@@ -149,6 +174,7 @@ TVMStatus VMThreadSleep(TVMTick tick)
   }// the current process yields the remainder of its processing quantum to the next ready process of equal priority.
   else if (tick == VM_TIMEOUT_INFINITE)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }//invalid param
   else
@@ -157,23 +183,28 @@ TVMStatus VMThreadSleep(TVMTick tick)
     tr->setState(VM_THREAD_STATE_WAITING);
     scheduler();
   } //does nothing while the number of ticks that have passed since entering this function is less than the number to sleep on
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadSleep(TVMTick tick)
 
 
 TVMStatus VMThreadID(TVMThreadIDRef threadref)
 {
+  MachineSuspendSignals(&sigs);
   if (!threadref)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }//if threadref is a null pointer
   threadref = tr->getIDRef();
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadID(TVMThreadIDRef threadref)
 
 
 TVMStatus VMThreadState(TVMThreadID thread, TVMThreadStateRef state)
 {
+  MachineSuspendSignals(&sigs);
   bool found = false;
   if (!state)
   {
@@ -188,13 +219,18 @@ TVMStatus VMThreadState(TVMThreadID thread, TVMThreadStateRef state)
     }//found correct thread
   }//linear search through threads vector for the thread ID in question
   if (!found)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_ID;
+  }//if thread wasn't a valid ID
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadState(TVMThreadID thread, TVMThreadStateRef state)
 
 
 TVMStatus VMThreadActivate(TVMThreadID thread)
 {
+  MachineSuspendSignals(&sigs);
   bool found = false;
   Thread* nt = NULL;
   for (vector<Thread*>::iterator itr = threads->begin(); itr != threads->end(); itr++)
@@ -206,21 +242,30 @@ TVMStatus VMThreadActivate(TVMThreadID thread)
     }//found correct thread
   }//linear search through threads vector for the thread ID in question
   if (!found)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_ID;
+  }//if thread ID doesn't exist
   if (nt->getState() != VM_THREAD_STATE_DEAD)
+  {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_STATE;
+  }//if thread is already dead
   nt->setState(VM_THREAD_STATE_READY);
   nt->getEntry();
   readyQ[nt->getPriority()]->push(nt);
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadActivate(TVMThreadID thread)
 
 
 TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsize, TVMThreadPriority prio, TVMThreadIDRef tid)
 {
+  MachineSuspendSignals(&sigs);
   SMachineContext context;
   if (!entry || !tid)
   {
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_PARAMETER;
   }//INVALID PARAMS, must not be NULL
   uint8_t *mem =  new uint8_t[memsize];
@@ -228,18 +273,22 @@ TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsiz
   threads->push_back(t);
   MachineContextCreate(&context, entry, param, (void*) mem, memsize);
   t->setContext(context);
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsize, TVMThreadPriority prio, TVMThreadIDRef tid)
 
 
 TVMStatus VMThreadDelete(TVMThreadID thread)
 {
+  MachineSuspendSignals(&sigs);
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadDelete(TVMThreadID thread)
 
 
 TVMStatus VMThreadTerminate(TVMThreadID thread)
 {
+  MachineSuspendSignals(&sigs);
   Thread *term = NULL, *temp;
   int target;
   for (vector<Thread*>::iterator itr = threads->begin(); itr != threads->end(); itr++)
@@ -249,12 +298,12 @@ TVMStatus VMThreadTerminate(TVMThreadID thread)
   }//search through threads to find correct Thread* to update
   if (!term)
   {
-    scheduler();
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_ID;
   }//term not found in threads
   if (term->getState() == VM_THREAD_STATE_DEAD)
   {
-    scheduler();
+    MachineResumeSignals(&sigs);
     return VM_STATUS_ERROR_INVALID_STATE;
   }//requested thread already dead
   term->setState(VM_THREAD_STATE_DEAD);
@@ -268,6 +317,7 @@ TVMStatus VMThreadTerminate(TVMThreadID thread)
       readyQ[term->getPriority()]->push(temp);
   }//cycle the relevant ready queue completely to find and flush the terminated thread from the ready queue
   scheduler();
+  MachineResumeSignals(&sigs);
   return VM_STATUS_SUCCESS;
 }//TVMStatus VMThreadTerminate(TVMThreadID thread)
 
@@ -284,7 +334,6 @@ void fileCallback(void* calldata, int result)
 
 void timerISR(void*)
 {
-//  MachineResumeSignals(&sigs);
   MachineSuspendSignals(&sigs);
   for (vector<Thread*>::iterator itr = threads->begin(); itr != threads->end(); itr++)
   {
@@ -297,6 +346,8 @@ void timerISR(void*)
 
 void scheduler()
 {
+  MachineResumeSignals(&sigs);
+  MachineSuspendSignals(&sigs);
   pt = tr;
   if (pt->getState() == VM_THREAD_STATE_RUNNING)
   {
@@ -324,6 +375,7 @@ void scheduler()
     readyQ[VM_THREAD_PRIORITY_NIL]->pop();
   }//if there's nothing in any of the RQs, spin with the idle process
   tr->setState(VM_THREAD_STATE_RUNNING);
+  MachineResumeSignals(&sigs);
   MachineContextSwitch(pt->getContextRef(), tr->getContextRef());
 }//void scheduler()
 
